bb3ca8a9e88f406aecbe2e3d90e1750f1da9ac2d
KavirSingh11 <kavir.singh11@gmail.com>
2020-07-21 09:20:56 -0400

    client setup added

--- server/models/User.js
+++ server/models/User.js
@@ -1,40 +1,41 @@
-const mongoose = require('mongoose');
-const joi = require('joi');
+const mongoose = require("mongoose");
+const joi = require("joi");
 const UserSchema = new mongoose.Schema({
-    email: {
-        type: String,
-        required: true
-    },
-    name: {
-        type: String
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    date: {
-        type: Date,
-        default: Date.now
-    }
+	email: {
+		type: String,
+		required: true,
+	},
+	name: {
+		type: String,
+		required: true,
+	},
+	password: {
+		type: String,
+		required: true,
+	},
+	date: {
+		type: Date,
+		default: Date.now,
+	},
 });
 
-function validateSignUp(user){
-    const schema = {
-        email: joi.string().required().email(),
-        name: joi.string().required(),
-        password: joi.string().required()
-    };
-    return joi.validate(user, schema);
+function validateSignUp(user) {
+	const schema = {
+		email: joi.string().required().email(),
+		name: joi.string().required(),
+		password: joi.string().required(),
+	};
+	return joi.validate(user, schema);
 }
-function validateLogIn(user){
-    const schema = {
-        email: joi.string().required().email(),
-        password: joi.string().required()
-    };
-    return joi.validate(user, schema);
+function validateLogIn(user) {
+	const schema = {
+		email: joi.string().required().email(),
+		password: joi.string().required(),
+	};
+	return joi.validate(user, schema);
 }
 
-const User = mongoose.model('user', UserSchema);
+const User = mongoose.model("user", UserSchema);
 
 exports.User = User;
 exports.validateSignUp = validateSignUp;
